6a7fa682b126d41da08ac022778487f725490a75
buryattvoydrug <buryattvoydrug@gmail.com>
2021-03-30 16:18:17 +0300

    VIGENERE готово

--- vigenere.py
+++ vigenere.py
@@ -5,23 +5,6 @@ def encrypt_vigenere(plaintext: str, keyword: str) -> str:
     letters = string.ascii_letters
     abc = letters[:len(letters)//2]+letters[:len(letters)//2]
     ABC = letters[len(letters)//2:]+letters[len(letters)//2:]
-    shift = ord("L")+ord("A")
-    ciphertext = ''.join((ABC[ABC.find(plaintext[i])+ABC.find(keywordToText[i])])
-                         if plaintext[i].isupper()
-                         else abc[abc.find(plaintext[i])+abc.find(keywordToText[i])]
-                         for i in range(len(plaintext)))
-
-    return ciphertext
-
-
-def encrypt_vigenere(plaintext: str, keyword: str) -> str:
-    import string
-    keywordToText = ''.join(keyword[i % len(keyword)]
-                            for i in range(len(plaintext)))
-    letters = string.ascii_letters
-    abc = letters[:len(letters)//2]+letters[:len(letters)//2]
-    ABC = letters[len(letters)//2:]+letters[len(letters)//2:]
-    shift = ord("L")+ord("A")
     ciphertext = ''.join((ABC[ABC.find(plaintext[i])+ABC.find(keywordToText[i])])
                          if plaintext[i].isupper()
                          else abc[abc.find(plaintext[i])+abc.find(keywordToText[i])]
@@ -33,3 +16,22 @@ def encrypt_vigenere(plaintext: str, keyword: str) -> str:
 print(encrypt_vigenere("PYTHON", "A"))
 print(encrypt_vigenere("python", "a"))
 print(encrypt_vigenere("ATTACKATDAWN", "LEMON"))
+
+
+def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
+    import string
+    keywordToText = ''.join(keyword[i % len(keyword)]
+                            for i in range(len(ciphertext)))
+    letters = string.ascii_letters
+    abc = letters[:len(letters)//2]+letters[:len(letters)//2]
+    ABC = letters[len(letters)//2:]+letters[len(letters)//2:]
+    plaintext = ''.join((ABC[ABC.find(ciphertext[i])-ABC.find(keywordToText[i])])
+                        if ciphertext[i].isupper()
+                        else abc[abc.find(ciphertext[i])-abc.find(keywordToText[i])]
+                        for i in range(len(ciphertext)))
+    return plaintext
+
+
+print(decrypt_vigenere("PYTHON", "A"))
+print(decrypt_vigenere("python", "a"))
+print(decrypt_vigenere("LXFOPVEFRNHR", "LEMON"))

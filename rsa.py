c34a4d64d8643c6578e319972e1da1f2cfc32041
buryattvoydrug <buryattvoydrug@gmail.com>
2021-03-30 18:25:01 +0300

    Реализована функция поиска НОД

--- rsa.py
+++ rsa.py
@@ -6,3 +6,41 @@ def is_prime(n: int) -> bool:
             continue
     return True
     pass
+
+
+def gcd(a: int, b: int) -> int:
+    while a != 0 and b != 0:
+        if a > b:
+            a = a % b
+        else:
+            b = b % a
+    return a+b
+    pass
+
+
+def generate_keypair(p: int, q: int) -> Tuple[Tuple[int, int], Tuple[int, int]]:
+    if not (is_prime(p) and is_prime(q)):
+        raise ValueError('Both numbers must be prime.')
+    elif p == q:
+        raise ValueError('p and q cannot be equal')
+
+    # n = pq
+    # PUT YOUR CODE HERE
+
+    # phi = (p-1)(q-1)
+    # PUT YOUR CODE HERE
+
+    # Choose an integer e such that e and phi(n) are coprime
+    e = random.randrange(1, phi)
+
+    # Use Euclid's Algorithm to verify that e and phi(n) are comprime
+    g = gcd(e, phi)
+    while g != 1:
+        e = random.randrange(1, phi)
+        g = gcd(e, phi)
+
+    # Use Extended Euclid's Algorithm to generate the private key
+    d = multiplicative_inverse(e, phi)
+    # Return public and private keypair
+    # Public key is (e, n) and private key is (d, n)
+    return ((e, n), (d, n))
